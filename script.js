280d889759b95e3c71ae991daded21cc2b192919
MAARAN <117466056+realmaaran@users.noreply.github.com>
2024-07-07 15:20:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 90)
+})
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('#navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('navlist-open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('navlist-open');
+};
